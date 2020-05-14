68f6e67ed6ac26481f8257b80fa31bb7049184ba
Anderson <anderson@ferneda.com.br>
2020-05-14 09:00:57 -0300

    Função de primeira classe

--- programacao_funcional/funcao_primeira_classe.py
+++ programacao_funcional/funcao_primeira_classe.py
@@ -8,6 +8,12 @@ def quadrado(x):
 
 
 if __name__ == '__main__':
+    d = dobro
+    print(d(5))
+
+    q = quadrado
+    print(q(5))
+
     # Retornar alternadamente o dobro ou o quadrado nos números de 1 a 10
     funcs = [dobro, quadrado] * 5
     for func, numero in zip(funcs, range(1, 11)):
