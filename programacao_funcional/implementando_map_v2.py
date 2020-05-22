b8b6dd66de6f12dd0cc5eb62f5d40156822b8162
Anderson <anderson@ferneda.com.br>
2020-05-21 23:53:59 -0300

    implemenando map v2

---
+++ programacao_funcional/implementando_map_v2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# implementação simplificada do map
+
+
+def mapear(funcao, lista):
+    return (funcao(elemento) for elemento in lista)
+
+if __name__ == '__main__':
+    print(list(mapear(lambda x: x ** 2, [2, 3, 4])))
