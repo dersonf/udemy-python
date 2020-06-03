339c3fbcf650385a2e67bf7d11afe51d512c436e
Anderson <anderson@ferneda.com.br>
2020-06-03 18:55:22 -0300

    Exluir tabela

--- banco_dados/excluir_tabela.py
+++ banco_dados/excluir_tabela.py
@@ -1,7 +1,14 @@
 from bd import nova_conexao
+from mysql.connector import ProgrammingError
+
 
 apaga_tabela = """
-    DROPY TABLE emails
+    DROP TABLE IF EXISTS emails
 """
 
-
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(apaga_tabela)
+    except ProgrammingError as e:
+        print(f'Erro: {e.msg}')
