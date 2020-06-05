89410e9437c01baf5b691f46f05e664c66871bc3
Anderson <anderson@ferneda.com.br>
2020-06-05 15:13:57 -0300

    Banco de dados

--- banco_dados/sqllite.py
+++ banco_dados/sqllite.py
@@ -17,9 +17,40 @@ tabela_contatos = """
     )
 """
 
-insert_grupos = 'INSER INTO (descricao) VALUES (?)'
+insert_grupos = 'INSERT INTO grupos (descricao) VALUES (?)'
 select_grupos = 'SELECT id, descricao FROM grupos'
 insert_contatos = 'INSERT INTO contatos (nome, tel, grupo_id) VALUES (?, ?, ?)'
 select = """
-    SELECT grupos.descricao AS grupo,
+    SELECT grupos.descricao AS grupo, contatos.nome AS contato
+    FROM contatos
+    INNER JOIN grupos ON contatos.grupo_id = grupos.id
+    ORDER BY grupo, contato
+"""
 
+try:
+    conexao = connect('memory:')
+    conexao.row_factory = Row
+
+    cursor = conexao.cursor()
+    cursor.execute(tabela_grupo)
+    cursor.execute(tabela_contatos)
+
+    cursor.executemany(insert_grupos, (('Casa',), ('Trabalho',)))
+    cursor.execute(select_grupos)
+    grupos = {row['descricao']: row['id'] for row in cursor.fetchall()}
+
+    contatos = (
+        ('Arthur', 456, grupos['Casa']),
+        ('Paulo', 789, grupos['Casa']),
+        ('Ângelo', 000, grupos['Trabalho']),
+        ('Eduardo', 987, None),
+        ('Yuri', 654, None),
+        ('Leonardo', 321, None),
+    )
+    cursor.executemany(insert_contatos, contatos)
+
+    cursor.execute(select)
+    for contato in cursor:
+        print(contato['contato'], contato['grupo'])
+except ProgrammingError as e:
+    print(f'Erro: {e.msg}')
